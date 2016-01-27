b9999c50701803e0069282666bc4bd1c100425c6
Aniata Lehartel <lehartel.aniata@gmail.com>
2016-01-27 15:04:25 +0100

    maj

--- WebServiceCompteBanck/src/org/jboss/samples/daoDto/ProprietaireDAO.java
+++ WebServiceCompteBanck/src/org/jboss/samples/daoDto/ProprietaireDAO.java
@@ -1,5 +1,37 @@
 package org.jboss.samples.daoDto;
 
-public class ProprietaireDAO {
+import java.util.List;
+
+public class ProprietaireDAO implements Proprietaire {
+
+	@Override
+	public List<ProprietaireDAO> listerProprietaire() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProprietaireDAO afficherProprietaire(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean ajouterProprietaire(Proprietaire newProprio) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modifierProprietaire(Proprietaire updateProprio) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean supprimerProprietaire(Proprietaire deleteProprio) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
