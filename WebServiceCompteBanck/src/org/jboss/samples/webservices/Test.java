44f2d9acc7a29a65d32454dba2e381d9e1686735
gantispam <gael.antispam@gmail.com>
2016-01-28 08:56:39 +0100

    commit

---
+++ WebServiceCompteBanck/src/org/jboss/samples/webservices/Test.java
@@ -0,0 +1,25 @@
+package org.jboss.samples.webservices;
+
+import org.jboss.samples.daoDto.CompteDao;
+import org.jboss.samples.daoDto.CompteImpl;
+import org.jboss.samples.daoDto.DAOFactory;
+import org.jboss.samples.services.CompteCTRL;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		//CompteImpl comptee = new CompteDT
+		// FAIRE LE TEST INSERT ICI ???
+				
+				
+		//TEST DE LA CONNEXION
+		//DAOFactory.getInstance();
+		CompteImpl test = new CompteDao();
+		
+		//test.findAll();
+		//test.findById(1);
+		test.create();
+	}
+
+}
