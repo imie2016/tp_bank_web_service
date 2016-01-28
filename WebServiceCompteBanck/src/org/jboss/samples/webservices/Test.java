9e429743916d16d39138fadfb7e7e483c039ec6c
Steven <Steven.moulinet@gmail.com>
2016-01-28 11:12:26 +0100

    dd

--- WebServiceCompteBanck/src/org/jboss/samples/webservices/Test.java
+++ WebServiceCompteBanck/src/org/jboss/samples/webservices/Test.java
@@ -1,5 +1,10 @@
 package org.jboss.samples.webservices;
 
+import org.jboss.samples.daoDto.CompteDTO;
+import org.jboss.samples.daoDto.CompteDao;
+import org.jboss.samples.daoDto.DAO;
+import org.jboss.samples.daoDto.DAOConnection;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -7,7 +12,8 @@ public class Test {
 		//CompteImpl comptee = new CompteDT
 		// FAIRE LE TEST INSERT ICI ???
 				
-				
+		DAO<CompteDTO> test = new CompteDao(DAOConnection.getInstance());
+		System.out.println(test.findById(2));
 
 	}
 
